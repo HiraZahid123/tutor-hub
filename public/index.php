0dd5710536456f37e07a3fbae2c90b992880fad6
abidsabir1234 <abidj3477@gmail.com>
2026-07-31 17:40:12 +0500

    feat: add temporary request diagnostic logging to public index for WAF debugging

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+// Temporary WAF / 403 Diagnostic Log
+if (strpos($_SERVER['REQUEST_URI'] ?? '', 'register-tutor') !== false) {
+    $postClean = $_POST;
+    if (isset($postClean['bio'])) $postClean['bio'] = substr($postClean['bio'], 0, 20) . '...';
+    if (isset($postClean['teaching_experience'])) $postClean['teaching_experience'] = substr($postClean['teaching_experience'], 0, 20) . '...';
+    $logData = date('Y-m-d H:i:s') . ' | URI: ' . ($_SERVER['REQUEST_URI'] ?? '') . ' | Method: ' . ($_SERVER['REQUEST_METHOD'] ?? '') . ' | POST: ' . json_encode($postClean) . ' | Files: ' . json_encode(array_keys($_FILES ?? [])) . "\n";
+    file_put_contents(__DIR__ . '/waf_test.log', $logData, FILE_APPEND);
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
